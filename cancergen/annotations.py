422b0feb7e94a91a0e1236876ec5296a1e5d69dc
Bruno Grande <bgrande@sfu.ca>
2015-02-03 11:28:42 -0800

    optimized implementation of get_or_create

--- cancergen/annotations.py
+++ cancergen/annotations.py
@@ -10,7 +10,7 @@ class Gene(Base):
     __tablename__ = "gene"
 
     id = Column(Integer, primary_key=True)
-    gene_ensembl_id = Column(String(length=50))
+    gene_ensembl_id = Column(String(length=50), index=True, nullable=False)
     gene_symbol = Column(String(length=50))
     biotype = Column(String(length=50))
     chrom = Column(String(length=50))
@@ -26,33 +26,38 @@ class Transcript(Base):
     __tablename__ = "transcript"
 
     id = Column(Integer, primary_key=True)
-    transcript_ensembl_id = Column(String(length=50))
-    gene_id = Column(Integer, ForeignKey("gene.id"))
+    transcript_ensembl_id = Column(String(length=50), index=True, nullable=False)
+    gene_id = Column(Integer, ForeignKey("gene.id"), nullable=False)
     cds_start_pos = Column(Integer)
     cds_end_pos = Column(Integer)
     length = Column(Integer)
 
     gene = relationship("Gene", backref="transcripts")
 
+    unique_on = ["transcript_ensembl_id"]
+
+
 class Exon(Base):
     """Model for exon annotations"""
     __tablename__ = "exon"
 
     id = Column(Integer, primary_key=True)
-    exon_ensembl_id = Column(String(length=50))
-    gene_id = Column(Integer, ForeignKey("gene.id"))
-    transcript_id = Column(Integer, ForeignKey("transcript.id"))
+    exon_ensembl_id = Column(String(length=50), index=True, nullable=False)
+    gene_id = Column(Integer, ForeignKey("gene.id"), nullable=False)
+    transcript_id = Column(Integer, ForeignKey("transcript.id"), nullable=False)
     transcript_start_pos = Column(Integer)
     transcript_end_pos = Column(Integer)
     genome_start_pos = Column(Integer)
     genome_end_pos = Column(Integer)
     length = Column(Integer)
-    strand = Column(Enum("+", "-"))
+    strand = Column(Enum("1", "-1"))
     phase = Column(Enum("-1", "0", "1", "2"))
     end_phase = Column(Enum("-1", "0", "1", "2"))
 
     gene = relationship("Gene", backref="exons")
     gene = relationship("Transcript", backref="exons")
+
+    unique_on = ["exon_ensembl_id"]
 
 
 class Protein(Base):
@@ -60,10 +65,12 @@ class Protein(Base):
     __tablename__ = "protein"
 
     id = Column(Integer, primary_key=True)
-    protein_ensembl_id = Column(String(length=50))
-    gene_id = Column(Integer, ForeignKey("gene.id"))
-    transcript_id = Column(Integer, ForeignKey("transcript.id"))
-    nucl_length = Column(Integer)
+    protein_ensembl_id = Column(String(length=50), index=True, nullable=False)
+    gene_id = Column(Integer, ForeignKey("gene.id"), nullable=False)
+    transcript_id = Column(Integer, ForeignKey("transcript.id"), nullable=False)
+    cds_length = Column(Integer)
 
     gene = relationship("Gene", backref="proteins")
     gene = relationship("Transcript", backref="proteins")
+
+    unique_on = ["protein_ensembl_id"]
--- cancergen/base.py
+++ cancergen/base.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python
 
+from sqlalchemy import text
 from sqlalchemy.ext.declarative import declarative_base
 from exceptions import *
+import time
 
 
 class BaseMixin(object):
     """Provides methods to Base"""
 
+    @classmethod
     def get_or_create(cls, session, **kwargs):
         """If a model's unique_on class attribute is defined,
         get_or_create will first search the database for an
         existing instance based on the attributes listed in
         unique_on. Otherwise, it will create an instance.
+        Note that the session still needs to be committed.
         """
         # Check if class' unique_on is defined
         try:
             cls.unique_on
-        except NameError:
-            raise UndefinedUniqueOn("The {} class doesn't have a defined unique_on "
-                                    "class attribute.".format(cls.__name__))
+        except AttributeError:
+            raise UndefinedUniqueOnError("The {} class doesn't have a defined unique_on "
+                                         "class attribute.".format(cls.__name__))
         # Create subset of kwargs according to unique_on
-        unique_kwargs = {}
-        for key, value in [(k, v) for k, v in kwargs.iteritems() if k in cls.unique_on]:
-            unique_kwargs[key] = value
-        # Create string for filter
-        pass
+        unique_kwargs = {k: v for k, v in kwargs.iteritems() if k in cls.unique_on}
         # Check if instance already exists based on unique_kwargs
-        results = session.query(cls).filter_by(**unique_kwargs)
-        if len(results) > 1:
+        count = session.query(cls).filter_by(**unique_kwargs).count()
+        if count > 1:
             # Ambiguous unique instances
             raise MultipleUniqueInstancesError(
                 "More than one instance returned when looking for unique instance.")
-        elif len(results) == 1:
-            # Instance already exists, so return it
-            instance = results
-        elif len(results) == 0:
+        elif count == 1:
+            # Instance already exists; obtain it and return it
+            instance = session.query(cls).filter_by(**unique_kwargs).first()
+        elif count == 0:
             # Instance doesn't already exist, so create a new one
             instance = cls(**kwargs)
             session.add(instance)
-            session.commit()
         return instance
 
 
