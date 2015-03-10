e6664a04b7fa764541449eb38aa906d2ffe7d73e
Bruno Grande <contact@brunogrande.me>
2015-03-09 20:37:42 -0700

    forgot to bump version

--- cancer_api/__init__.py
+++ cancer_api/__init__.py
@@ -4,7 +4,7 @@ __init__.py
 Where it all begins.
 """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from main import connect, get_connection, set_connection
 from connections import MysqlConnection, SqliteConnection
