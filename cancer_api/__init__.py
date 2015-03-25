b2faf8e0c6dcd689eda72bda7cc9dc334431d4e8
Bruno Grande <bgrande@sfu.ca>
2015-03-24 17:03:16 -0700

    moved things in init around

--- cancer_api/__init__.py
+++ cancer_api/__init__.py
@@ -4,8 +4,6 @@ __init__.py
 Where it all begins.
 """
 
-__version__ = "0.2.0"
-
 from main import connect, get_connection, set_connection
 from connections import MysqlConnection, SqliteConnection
 from mutations import SingleNucleotideVariant, Indel, CopyNumberVariation, StructuralVariation
@@ -14,3 +12,5 @@ from annotations import Gene, Transcript, Exon, Protein
 from files import *
 from parsers import *
 from utils import *
+
+__version__ = "0.2.0"
